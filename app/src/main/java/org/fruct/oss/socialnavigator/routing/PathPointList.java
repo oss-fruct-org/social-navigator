57c9bb7f809bd317ea554fcb0dd15173543050d3
Kirill Ivashov <ivashov@cs.karelia.ru>
2014-10-07 18:49:36 +0400

    Switch old PointList to new generic implementation

--- app/src/main/java/org/fruct/oss/socialnavigator/routing/PathPointList.java
+++ app/src/main/java/org/fruct/oss/socialnavigator/routing/PathPointList.java
@@ -6,6 +6,8 @@ import com.graphhopper.GHResponse;
 import com.graphhopper.util.InstructionList;
 import com.graphhopper.util.PointList;
 
+import org.fruct.oss.socialnavigator.utils.EarthSpace;
+import org.fruct.oss.socialnavigator.utils.Space;
 import org.fruct.oss.socialnavigator.utils.Turn;
 import org.fruct.oss.socialnavigator.utils.Utils;
 import org.osmdroid.api.IGeoPoint;
@@ -22,22 +24,14 @@ public class PathPointList implements Iterable<GeoPoint> {
 	public static final double DISTANCE_DEVIATE = 20;
 	public static final double DISTANCE_NEAR = 20;
 
-	private Deque<GeoPoint> points = new ArrayDeque<GeoPoint>();
-	private GeoPoint currentLocation;
-	private GeoPoint p1;
-	private GeoPoint matchedPoint = new GeoPoint(0, 0);
-
-	private double lastDeviation;
-
-	private double[] tmpDouble = new double[2];
-	private int[] tmpInt = new int[2];
+	private org.fruct.oss.socialnavigator.utils.PointList pointList
+			= new org.fruct.oss.socialnavigator.utils.PointList(new EarthSpace(), DISTANCE_NEAR);
 
 	public PathPointList(GHResponse response) {
-		PointList pointList = response.getPoints();
-		InstructionList instructions = response.getInstructions();
+		PointList ghPointList = response.getPoints();
 
-		for (int i = 0; i < pointList.size(); i++) {
-			points.add(new GeoPoint(pointList.getLatitude(i), pointList.getLon(i)));
+		for (int i = 0; i < ghPointList.size(); i++) {
+			pointList.addPoint(ghPointList.getLatitude(i), ghPointList.getLon(i));
 		}
 	}
 
@@ -45,64 +39,43 @@ public class PathPointList implements Iterable<GeoPoint> {
 	}
 
 	public void addPoint(double lat, double lon) {
-		points.add(new GeoPoint(lat, lon));
+		pointList.addPoint(lat, lon);
 	}
 
 	public boolean isDeviated() {
-		return lastDeviation >= DISTANCE_DEVIATE;
+		return pointList.isDeviated();
 	}
 
 	public boolean isCompleted() {
-		return !isDeviated() && points.size() == 1 && currentLocation.distanceTo(points.getFirst()) < DISTANCE_NEAR;
+		return pointList.isCompleted();
 	}
 
 	public void setLocation(Location location) {
-		currentLocation = new GeoPoint(location);
-
-		if (p1 == null) {
-			p1 = points.pollFirst();
-
-			if (points.size() < 2) {
-				return;
-			}
-		}
-
-		GeoPoint p2 = points.peekFirst();
-
-		lastDeviation = Utils.calcDist(location.getLatitude(), location.getLongitude(),
-				p1.getLatitude(), p1.getLongitude(),
-				p2.getLatitude(), p2.getLongitude(), tmpInt, tmpDouble);
-
-		matchedPoint.setCoordsE6((int) (tmpDouble[0] * 1e6), (int) (tmpDouble[1] * 1e6));
-
-		// Location pass second point of current segment
-		if ((tmpInt[0] == 2 || p2.distanceTo(currentLocation) < DISTANCE_NEAR) && points.size() > 1) {
-			p1 = null;
-			setLocation(location);
-		}
+		pointList.setLocation(location.getLatitude(), location.getLongitude());
 	}
 
 	@Override
 	public Iterator<GeoPoint> iterator() {
 		return new Iterator<GeoPoint>() {
-			private boolean first = p1 != null;
-			private Iterator<GeoPoint> iter = points.iterator();
+			private Iterator<Space.Point> spacePointsIterator = pointList.iterator();
 
 			@Override
 			public boolean hasNext() {
-				return first || iter.hasNext();
+				return spacePointsIterator.hasNext();
 			}
 
 			@Override
 			public GeoPoint next() {
-				GeoPoint ret = first ? matchedPoint : iter.next();
-				first = false;
-				return ret;
+				Space.Point p = spacePointsIterator.next();
+				if (p == null)
+					return null;
+				else
+					return new GeoPoint(p.x, p.y);
 			}
 
 			@Override
 			public void remove() {
-				throw new UnsupportedOperationException("Can't remove");
+				spacePointsIterator.remove();
 			}
 		};
 	}
---
+++ app/src/main/java/org/fruct/oss/socialnavigator/utils/EarthSpace.java
@@ -0,0 +1,39 @@
+package org.fruct.oss.socialnavigator.utils;
+
+import android.location.Location;
+
+import org.osmdroid.util.GeoPoint;
+
+public class EarthSpace implements Space {
+	private float[] ret = new float[1];
+	private double[] tmpDouble = new double[2];
+
+	private GeoPoint p1 = new GeoPoint(0, 0);
+	private GeoPoint p2 = new GeoPoint(0, 0);
+
+	@Override
+	public Point newPoint(double x, double y) {
+		return new Point(x, y);
+	}
+
+	@Override
+	public double dist(Point a, Point b) {
+		Location.distanceBetween(a.x, a.y, b.x, b.y, ret);
+		return ret[0];
+	}
+
+	@Override
+	public double bearing(Point a, Point b) {
+		p1.setCoordsE6((int) (a.x * 1e6), (int) (a.y * 1e6));
+		p2.setCoordsE6((int) (b.x * 1e6), (int) (b.y * 1e6));
+		return p1.bearingTo(p2);
+	}
+
+	@Override
+	public double projectedDist(Point r, Point a, Point b, int[] tmpInt, Point out) {
+		double ret = Utils.calcDist(r.x, r.y, a.x, a.y, b.x, b.y, tmpInt, tmpDouble);
+		out.x = tmpDouble[0];
+		out.y = tmpDouble[1];
+		return ret;
+	}
+}
